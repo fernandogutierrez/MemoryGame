4254e987929775d7e043bd5ee6e360179e6a23d6
belenluz <Lucero.Penarrieta@fundacion-jala.org>
2016-02-05 19:39:51 -0400

    This is a commit that describes changes on Player class.

--- js/Table.js
+++ js/Table.js
@@ -1,27 +1,39 @@
 /**
  * Created by LuceroPenarrieta on 2/4/2016.
  */
+
+
 var Table=function(){
     var iMaxNum = 5;
     var i, j;
-    var newArray = new Array(iMaxNum + 1);
-    for (i = 1; i <= iMaxNum; i++)
-    {
-        newArray[i] = new Array(iMaxNum + 1);
-        for (j = 1; j <= iMaxNum; j++)
-        {
-            newArray[i][j] = "*";
-            console.log(newArray[i][j]);
+    var v;
+    var symbols=['*','$','@','#','%'];
+    this.newArray = [];//new Array(iMaxNum - 1);
 
+
+    var getRandom= function(){
+        return Math.floor((Math.random()*5));
+    };
+    for (i = 0; i <= iMaxNum; i++)
+    {
+        this.newArray[i] = [];
+        for (j = 0; j <= iMaxNum; j++)
+        {
+            this.newArray[i][j] = symbols[getRandom()];
         }
     }
 
     this.display=function(){
-        for (i = 1; i <= iMaxNum; i++){
-            for (j = 1; j <= iMaxNum; j++)
-            {
-                console.log(newArray[i][j].concat("*"));
-            }
+        for (i = 0; i <= iMaxNum; i++){
+            //for (j = 0; j <= iMaxNum; j++)
+            //{
+            //    v=this.newArray[i][j];
+            //}
+            //console.log(newArray[i]);
+            console.log(i, this.newArray[i].join(' - '));
         }
     };
+
+
+
 };
