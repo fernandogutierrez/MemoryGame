05c631bd9de4c0babc4e34a42e5da32b228b0994
belenluz <Lucero.Penarrieta@fundacion-jala.org>
2016-02-08 00:21:19 +0100

    This is a commit that describes some changes in the Element class.

--- js/element.js
+++ js/element.js
@@ -1,14 +1,28 @@
-var Element = function () {
-	var status='hidden';
-	var value;
-	var posX;
-	var posY;
+﻿var Element = function () {
+    	var status='hidden';
+    	var value;
+    	var posX;
+    	var posY;
+        var symbols=['*','$','@','#','%'];
 
-	this.getStatus = function (argument) {
-		return status;
-	}
-    this.setStatus = function (s) {
-         status = s;    
-    }
 
-}
+    	this.getRandom = function () {
+    	    return Math.floor((Math.random() * 5));
+    	};
+
+    	this.generateValue = function () {
+    	    value = symbols[this.getRandom()];
+    	    this.display();
+    	};
+    
+    	this.getStatus = function (argument) {
+        		return status;
+        }
+        this.setStatus = function (s) {
+                status = s;    
+        }
+        this.display = function () {
+            console.log(value);
+        };
+    
+    };
