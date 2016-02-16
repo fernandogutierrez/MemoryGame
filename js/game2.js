68b40852dc6450f4c9bb01841db6fabbd8560e50
Administrator <Teresa.Flores@Fundacion-jala.org>
2016-02-15 20:48:55 -0400

    Improve Class Game

---
+++ js/game2.js
@@ -0,0 +1,32 @@
+var imported = document.createElement('script');
+imported.src = 'C:/Users/FernandoGutierrez/Desktop/Solutions/JavaScript2/MemoryGame/js/table.js';
+document.head.appendChild(imported);
+
+var Game = function(namePlayer)
+{
+    var tries;
+    var score;
+    var table;
+     /**
+     *
+     * @param namePlayer this string recive the name of the player
+     */    
+
+    var player = new Player(namePlayer);
+
+    this.decrementTries = function(){
+      tries--;
+      }
+    this.endGame = function()
+      {
+      return score;
+      }
+    this.beginGame = function (argument) {
+      table = new Table(player.getSize());      	 
+    }
+    this.discover = function (posX,posY) {
+    	table.discover(posX,posY);
+    }
+   
+
+};
