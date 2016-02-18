cf37f58dc1696cb6746736aa129249194f62c2fb
Fernando <fernandito_727@hotmail.com>
2016-02-17 20:21:57 -0400

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,33 +1,40 @@
 
 var name = prompt("What is your name?");
-var player = new Player(name);
-var sizeTable = prompt("Insert the size");
+var sizeTable = prompt("Insert the size for the table");
 
-var numberOfFields = (sizeTable + 1) * (sizeTable + 1);
-var numberOfPairs = Math.floor(numberOfFields/2);
-var elements = []; 
-	while (numberOfPairs>0)
-	{
-         var el = new Element();
-         el.generateValue();
-         elements.push(el.getValue());
-         numberOfPairs--;
-	}
+var player = new Player(name,sizeTable);
+var game = new Game(player);
 
-var table = new Table(sizeTable,elements);
-console.log(elements);
+var table = game.beginGame();
+var elementDiscovered = "";
+var lastElement= "";
+while(game.getTries()>0)
+{
 
+  var posX = prompt("Insert a position X to discover");
+  var posY = prompt("Insert a position Y to discover");
+  
+  table.discoverElement(posX,posY);
 
+  elementDiscovered = table.getElementDiscovered(); //object element
 
+  if (lastElement != "") {
 
+  		if (lastElement.getValue() === elementDiscovered.getValue()) { 
 
+         	game.incrementScore()
 
-
-var element =  new Element();
-element.generateValue();
-var value = element.getValue();
-table.fillPairs(value);
-
+     	}
+     	else{
+     		table.hide(lastElement);
+        	table.hide(elementDiscovered);
+     	}
+  	
+  }
+  lastElement = table.getElementDiscovered();
+  game.decrementTries();
+}
+console.log("The score is : "+ game.endGame());
 
 
 
