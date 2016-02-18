f184695263ee29a604f8b633640a341f583ba09e
Fernando <fernandito_727@hotmail.com>
2016-02-17 20:21:22 -0400

    Update game.js

--- js/game.js
+++ js/game.js
@@ -1,30 +1,40 @@
-var imported = document.createElement('script');
-imported.src = 'C:/Users/FernandoGutierrez/Desktop/Solutions/JavaScript2/MemoryGame/js/table.js';
-document.head.appendChild(imported);
 
-var Game = function(namePlayer)
+
+var Game = function(player)
 {
-	var tries;
-    var score;
+
+    var namePlayer =  player.getName();
+    var sizeTable = player.getSize();
+
+    var tries = sizeTable*2-2;
+    var score = 0;
     var table;
+     /**
+     *
+     * @param namePlayer this string recive the name of the player
+     */    
 
-    
-    var player = new Player(namePlayer);
+    var player = new Player(namePlayer,sizeTable);
 
-
-	this.decrementTries = function(){
-       tries--;
-	}
- 	this.endGame = function()
- 	{
-       return score;
- 	}
-    this.beginGame = function (argument) {
-        table = new Table(player.getSize());      	 
-    }
-    this.discover = function (posX,posY) {
-    	table.discover(posX,posY);
+    this.decrementTries = function(){
+      tries--;
     }
    
+    this.endGame = function()
+    {
+      return score;
+    }
+    this.beginGame = function () {
+      table = new Table(sizeTable);
+      return table;         
+    }
+    this.discover = function (posX,posY) {
+        table.discover(posX,posY);
+    }
+    this.incrementScore = function() {
+        score++;
+    }
+    
+
 
 };
